7920cff2b84719c142418f9996bdf98674b91689
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-04 14:48:43 +0300

    created static class with extensions methods

---
+++ Api/ServiceExtensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Interfaces;
+using Infastructure.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.ServiceExtensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => { });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
