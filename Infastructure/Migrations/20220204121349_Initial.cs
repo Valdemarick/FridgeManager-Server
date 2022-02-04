053ebbba050fa27d8abc8c9c535fa780d1eecf0c
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-04 15:18:00 +0300

    created a migration

---
+++ Infastructure/Migrations/20220204121349_Initial.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FridgeModels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    ProductionYear = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FridgeModels", x => x.Id);
+                    table.CheckConstraint("ProductionYear", "ProductionYear >= 1899 AND ProductionYear <= 2022");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: true, defaultValue: 1)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.CheckConstraint("Quantity", "Quantity > 0 AND Quantity < 10");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fridges",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OwnerName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true, defaultValue: "none"),
+                    FridgeModelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fridges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fridges_FridgeModels_FridgeModelId",
+                        column: x => x.FridgeModelId,
+                        principalTable: "FridgeModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fridges_Products",
+                columns: table => new
+                {
+                    FridgeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductQuantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fridges_Products", x => new { x.FridgeId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_Fridges_Products_Fridges_FridgeId",
+                        column: x => x.FridgeId,
+                        principalTable: "Fridges",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Fridges_Products_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fridges_FridgeModelId",
+                table: "Fridges",
+                column: "FridgeModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fridges_Products_ProductId",
+                table: "Fridges_Products",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fridges_Products");
+
+            migrationBuilder.DropTable(
+                name: "Fridges");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "FridgeModels");
+        }
+    }
+}
