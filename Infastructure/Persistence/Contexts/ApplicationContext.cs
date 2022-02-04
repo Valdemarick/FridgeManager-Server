c62566bf9896004a8e7bd6fa53886826f8603c95
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-04 15:04:31 +0300

    created db context

---
+++ Infastructure/Persistence/Contexts/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Infastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infastructure.Persistence.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Fridge> Fridges { get; set; }
+        public DbSet<FridgeModel> FridgeModels { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new FridgeModelConfiguration());
+            modelBuilder.ApplyConfiguration(new FridgeConfiguration());
+            modelBuilder.ApplyConfiguration(new FridgeProductConfiguration());
+        }
+    }
+}
