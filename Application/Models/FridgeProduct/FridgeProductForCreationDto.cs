bbfc0fbe89ea28e537be64c49c6c2987d8f76478
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-10 11:38:35 +0300

    created a dto for creation a fridgeproduct cover

---
+++ Application/Models/FridgeProduct/FridgeProductForCreationDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Application.Models.FridgeProduct
+{
+    public class FridgeProductForCreationDto
+    {
+        public Guid FridgeId { get; set; }
+        public Guid ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
