129e4bb811fc7c3d0509b83761736715bdf7cc86
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-10 11:37:23 +0300

    created a dto for creation a product

---
+++ Application/Models/Product/ProductForCreationDto.cs
@@ -0,0 +1,10 @@
+﻿#nullable enable
+
+namespace Application.Models.Product
+{
+    public class ProductForCreationDto
+    {
+        public string Name { get; set; } = null!;
+        public int? Quantity { get; set; }
+    }
+}
