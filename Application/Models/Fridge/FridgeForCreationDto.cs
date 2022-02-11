c37c533fafa927fed6a147bae270b0d30daa24ca
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-11 20:58:43 +0300

    created dto for creation fridge

---
+++ Application/Models/Fridge/FridgeForCreationDto.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+using System;
+
+namespace Application.Models.Fridge
+{
+    public class FridgeForCreationDto
+    {
+        public string? OwnerName { get; set; }
+        public Guid ModelId { get; set; }
+    }
+}
