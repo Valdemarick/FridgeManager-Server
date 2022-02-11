eb9f4aa0b247dd9582b13e01c1ef387a947b44d5
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-11 21:07:39 +0300

    created DELETE and POST methods

--- Api/Controllers/FridgesController.cs
+++ Api/Controllers/FridgesController.cs
@@ -34,8 +34,8 @@ namespace Api.Controllers
             return Ok(fridgeDtos);
         }
 
-        [Route("{id}")]
         [HttpGet]
+        [Route("{id}")]
         public async Task<IActionResult> GetFridgeById([FromRoute] Guid id)
         {
             var fridge = await _unitOfWork.Fridge.GetByIdAsync(id);
@@ -49,6 +49,43 @@ namespace Api.Controllers
             var fridgeDto = _mapper.Map<FridgeDto>(fridge);
 
             return Ok(fridgeDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFridge([FromBody] FridgeForCreationDto fridgeForCreationDto)
+        {
+            if (fridgeForCreationDto == null)
+            {
+                _logger.LogInfo($"The sent object is null");
+                return BadRequest();
+            }
+
+            var fridge = _mapper.Map<Fridge>(fridgeForCreationDto);
+
+            await _unitOfWork.Fridge.CreateAsync(fridge);
+            await _unitOfWork.SaveAsync();
+
+            var fridgeToReturn = _mapper.Map<FridgeDto>(fridge);
+
+            return CreatedAtRoute(nameof(GetFridgeById), new {fridgeToReturn.Id}, fridgeToReturn);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteFridgeById([FromRoute] Guid id)
+        {
+            var fridge = await _unitOfWork.Fridge.GetByIdAsync(id);
+
+            if (fridge == null)
+            {
+                _logger.LogInfo($"A fridge with id: {id} doesn't exist in the databse");
+                return BadRequest();
+            }
+
+            await _unitOfWork.Fridge.DeleteAsync(fridge.Id);
+            await _unitOfWork.SaveAsync();
+
+            return NoContent();
         }
     }
 }
