f44d099f8d7a75683d8b238e966f0647097105de
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-12 20:39:41 +0300

    created and added code from creation and updation because of code duplication

---
+++ Application/Models/Product/ProductForManipulation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+#nullable enable
+
+namespace Application.Models.Product
+{
+    public class ProductForManipulation
+    {
+        [Required(ErrorMessage = "'Name' property is a required field")]
+        [MaxLength(30, ErrorMessage = "Maximum length for 'Name' property is 30 characters")]
+        public string Name { get; set; } = null!;
+
+        [Range(0, 10, ErrorMessage = "'Quantity' property can't be less than 0 and more than 10'")]
+        public int? Quantity { get; set; }
+    }
+}
