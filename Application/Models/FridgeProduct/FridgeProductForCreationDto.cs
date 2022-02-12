9e1598b03e31820cca93c63e3f1f21d07036082e
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-12 21:21:09 +0300

    added constrainting attributes

--- Application/Models/FridgeProduct/FridgeProductForCreationDto.cs
+++ Application/Models/FridgeProduct/FridgeProductForCreationDto.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.Models.FridgeProduct
 {
     public class FridgeProductForCreationDto
     {
+        [Required(ErrorMessage ="'FridgeId' prperty is a required field")]
         public Guid FridgeId { get; set; }
+
+        [Required(ErrorMessage ="'ProductId' property is a required field")]
         public Guid ProductId { get; set; }
+
+        [Required(ErrorMessage ="'ProductQuantity' property is a required field")]
+        [Range(0, 10, ErrorMessage ="'ProductQuantity' property can't be less than 0 and more than 10")]
         public int ProductQuantity { get; set; }
     }
 }
