282fe2947b9ff07b5cfe9ae46092a2767bef8ec4
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-12 20:52:34 +0300

    created a class and  added code from update and create because of code duplication

---
+++ Application/Models/Fridge/FridgeForManipulation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+#nullable enable
+
+namespace Application.Models.Fridge
+{
+    public class FridgeForManipulation
+    {
+        [Required(ErrorMessage = "'ModelId' property is a required field")]
+        public Guid ModelId { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Maximum length for 'OwnerName' property is 50 characters")]
+        public string? OwnerName { get; set; }
+    }
+}
