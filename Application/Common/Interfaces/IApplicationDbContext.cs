57b30b1566f854bccbec7b098979dee76aacb4e4
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-14 18:38:01 +0300

    Created db context interface

---
+++ Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Fridge> Fridges { get; set; }
+        public DbSet<FridgeModel> FridgeModels { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<FridgeProduct> FridgeProducts { get; set; }
+
+        public DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        public Task<int> SaveChangesAsync();
+    }
+}
