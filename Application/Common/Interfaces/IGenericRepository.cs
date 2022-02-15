a4f08c6e0ef58cb679e3903133de690c6381f075
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-15 14:49:05 +0300

    added a ReadOnly version of GetById method

--- Application/Common/Interfaces/IGenericRepository.cs
+++ Application/Common/Interfaces/IGenericRepository.cs
@@ -8,6 +8,7 @@ namespace Application.Common.Interfaces
     public interface IGenericRepository<TEntity> where TEntity : BaseEntity
     {
         Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdReadOnlyAsync(Guid id);
         Task<TEntity> GetByIdAsync(Guid id);
         Task CreateAsync(TEntity entity);
         Task UpdateAsync(TEntity entity);
