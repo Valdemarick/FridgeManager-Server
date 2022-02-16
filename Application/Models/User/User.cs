11da46cad985077c7a3f747a69f469a841e0a9a6
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-16 16:06:31 +0300

    created user model

---
+++ Application/Models/User/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Application.Models.User
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
