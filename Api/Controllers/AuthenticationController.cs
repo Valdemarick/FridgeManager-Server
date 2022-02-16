4a4b13b6ba4134510cd3a6f1df33206adfba3b45
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-16 16:37:37 +0300

    created authentication controller

---
+++ Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using Application.Models.User;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+
+        public AuthenticationController(ILoggerManager logger, IMapper mapper, UserManager<User> userManager)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+    }
+}
