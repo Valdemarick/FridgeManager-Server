5f5a07eb4fde4e32df677e7a2a3d677b9384f235
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-16 17:44:27 +0300

    created a rule for user

---
+++ Application/Common/Mappings/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using Application.Models.User;
+using AutoMapper;
+
+namespace Application.Common.Mappings
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserForRegistrationDto, User>();
+        }
+    }
+}
