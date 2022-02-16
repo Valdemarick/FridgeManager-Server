9698be3c658702ef8171a7ed19cd0613c783b0a4
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-16 16:43:05 +0300

    created the model of user for registration

---
+++ Application/Models/User/UserForRegistrationDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.User
+{
+    public class UserForRegistrationDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public ICollection<string> Roles { get; set; }
+    }
+}
