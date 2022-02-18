c67343f54eb272407168f56553e7f65a3978c201
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-18 11:35:36 +0300

    created the authentication manager

---
+++ Application/Common/Interfaces/IAuthenticationManager.cs
@@ -0,0 +1,10 @@
+﻿using Application.Models.User;
+using System.Threading.Tasks;
+namespace Application.Common.Interfaces
+{
+    public interface IAuthenticationManager
+    {
+        Task<bool> ValidateUser(UserForAuthenticationDto userForAuthenticationDto);
+        Task<string> CreateToken();
+    }
+}
