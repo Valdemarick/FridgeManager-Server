de4fc7dd6524edf6a01d7f254fd3770935ec311c
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-18 11:10:22 +0300

    created a user model for authentication

---
+++ Application/Models/User/UserForAuthenticationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.User
+{
+    public class UserForAuthenticationDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set;}
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set;}
+    }
+}
