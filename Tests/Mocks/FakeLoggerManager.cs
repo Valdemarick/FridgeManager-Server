d3948c9ac10da31fe322e055677a67604bd5e416
Valdemarick <korolev.vadim.2017@mail.ru>
2022-02-21 22:46:02 +0300

    created mocks

---
+++ Tests/Mocks/FakeLoggerManager.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common.Interfaces;
+using Moq;
+
+namespace Tests.Mocks
+{
+    public class FakeLoggerManager
+    {
+        public Mock<ILoggerManager> Mock;
+        public ILoggerManager LoggerManager;
+
+        public FakeLoggerManager()
+        {
+            Mock = new Mock<ILoggerManager>();
+            LoggerManager = Mock.Object;
+        }
+    }
+}
---
+++ Tests/Mocks/FakeMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Moq;
+
+namespace Tests.Mocks
+{
+    public class FakeMapper
+    {
+        public Mock<IMapper> Mock;
+        public IMapper Mapper;
+
+        public FakeMapper()
+        {
+            Mock = new Mock<IMapper>();
+            Mapper = Mock.Object;
+        }
+    }
+}
