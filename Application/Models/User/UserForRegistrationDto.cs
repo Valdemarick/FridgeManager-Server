fa6bbbf03fb4b58867a265339ba88d313bbd3509
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-01 16:21:43 +0300

    added nullable types and added regex validation of email

--- Application/Models/User/UserForRegistrationDto.cs
+++ Application/Models/User/UserForRegistrationDto.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+#nullable enable
 
 namespace Application.Models.User
 {
     public class UserForRegistrationDto
     {
         [Required(ErrorMessage = "Username is required")]
-        public string UserName { get; set; }
+        public string UserName { get; set; } = null!;
 
         [Required(ErrorMessage = "Password is required")]
-        public string Password { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Email { get; set; }
-        public ICollection<string> Roles { get; set; }
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+" + 
+            @"(\.[a-z0-9_-]+)*.[a-z]{2,6}$", ErrorMessage = "You entered a wrong email address")]
+        public string Email { get; set; } = null!;
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public ICollection<string> Roles { get; set; } = null!;
     }
 }
