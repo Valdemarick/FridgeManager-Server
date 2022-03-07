29205b9b8de20c5e5dd4b00806aa265d68f7ea0d
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-07 14:25:05 +0300

    created 'FridgeModel' controller

---
+++ Api/Controllers/FridgeModelsController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interfaces;
+using Application.Models.FridgeModel;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/fridge-models")]
+    [ApiController]
+    public class FridgeModelsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public FridgeModelsController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet, Authorize]
+        public async Task<IActionResult> GetModelsAsync()
+        {
+            var models = await _unitOfWork.FridgeModel.GetAllAsync();
+            var modelsDtos = _mapper.Map<List<FridgeModelDto>>(models);
+
+            return Ok(modelsDtos);
+        }
+    }
+}
