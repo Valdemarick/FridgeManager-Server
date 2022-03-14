3d9582bbb12e26dd809fa83d4ecb12ab2e74c108
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-14 10:05:42 +0300

    created and implemented fridge model service

---
+++ Infastructure/Persistence/Services/FridgeModelService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces.Managers;
+using Application.Common.Interfaces.Services;
+using Application.Models.FridgeModel;
+using AutoMapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infastructure.Persistence.Services
+{
+    public class FridgeModelService : IFridgeModelService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public FridgeModelService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FridgeModelDto>> GetAllModelsAsync() =>
+             _mapper.Map<IEnumerable<FridgeModelDto>>(await _unitOfWork.FridgeModel.GetAllAsync());
+    }
+}
