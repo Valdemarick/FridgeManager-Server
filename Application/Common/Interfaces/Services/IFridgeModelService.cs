e1c46665665e54f60c8948d13b3439edd36d6dd3
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-18 19:36:28 +0300

    removed unused namespaces

--- Application/Common/Interfaces/Services/IFridgeModelService.cs
+++ Application/Common/Interfaces/Services/IFridgeModelService.cs
@@ -1,7 +1,5 @@
-﻿using System;
+﻿using Application.Models.FridgeModel;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.Common.Interfaces.Services
