8c9faf3576c9a0231807c06942d6ead52ac1ea97
Valdemarick <korolev.vadim.2017@mail.ru>
2022-03-23 22:19:02 +0300

    added pk property

--- Application/Models/FridgeProduct/FridgeProductDto.cs
+++ Application/Models/FridgeProduct/FridgeProductDto.cs
@@ -4,6 +4,7 @@ namespace Application.Models.Fridge
 {
     public class FridgeProductDto
     {
+        public Guid Id { get; set; }
         public Guid FridgeId { get; set; }
         public Guid ProductId { get; set; }
         public string ProductName { get; set; }
