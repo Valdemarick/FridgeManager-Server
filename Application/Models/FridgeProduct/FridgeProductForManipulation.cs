fc4c8c0eb0ebe97ee1199663d65b5dd720074f74
Valdemarick <korolev.vadim.2017@mail.ru>
2022-04-01 18:36:01 +0300

    created abstract class with general props, added inheritance from it to child classes

---
+++ Application/Models/FridgeProduct/FridgeProductForManipulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.FridgeProduct
+{
+    public abstract class FridgeProductForManipulation
+    {
+        [Required(ErrorMessage = "'FridgeId' prperty is a required field")]
+        public Guid FridgeId { get; set; }
+
+        [Required(ErrorMessage = "'ProductId' property is a required field")]
+        public Guid ProductId { get; set; }
+
+        [Required(ErrorMessage = "'ProductQuantity' property is a required field")]
+        [Range(0, 9, ErrorMessage = "'ProductQuantity' property can't be less than 0 and more than 9")]
+        public int ProductQuantity { get; set; }
+    }
+}
---
+++ Application/Models/FridgeProduct/FridgeProductForUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.FridgeProduct
+{
+    public class FridgeProductForUpdateDto
+    {
+        [Required(ErrorMessage = "Id is a required field")]
+        public Guid Id { get; set; }
+    }
+}
